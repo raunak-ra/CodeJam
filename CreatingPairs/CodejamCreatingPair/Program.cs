a366fa399c3efb5d60ff39ff2e41585d4fe46d02
Raunak singh <51930237+tavisca-rsingh@users.noreply.github.com>
2019-08-08 12:24:08 +0530

    Update Program.cs

--- CreatingPairs/CodejamCreatingPair/Program.cs
+++ CreatingPairs/CodejamCreatingPair/Program.cs
@@ -6,10 +6,93 @@ using System.Threading.Tasks;
 
 namespace CodejamCreatingPair
 {
-    class Program
+ class CreatePairs
     {
-        static void Main(string[] args)
+        public static int GetNegativeSum(List<int> negativeList, bool isZero)
         {
+            negativeList.Sort();
+            int sum = 0;
+            if (negativeList.Count % 2 == 0)
+            {
+                for (int i = 0; i < negativeList.Count; i += 2)
+                {
+                    sum += (negativeList[i] * negativeList[i + 1]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < negativeList.Count - 1; i += 2)
+                {
+                    sum += (negativeList[i] * negativeList[i + 1]);
+                }
+            }
+            if (isZero || (negativeList.Count % 2 == 0))
+            {
+                return sum;
+            }
+            return sum + negativeList[negativeList.Count - 1];
         }
+        public static int GetPositiveSum(List<int> positiveList)
+        {
+            positiveList.Sort();
+            int sum = 0;
+            if (positiveList.Count % 2 == 0)
+            {
+                for (int i = positiveList.Count - 1; i >= 0; i -= 2)
+                {
+                    sum += positiveList[i] * positiveList[i - 1];
+                }
+            }
+            else
+            {
+                for (int i = positiveList.Count - 1; i >= 1; i -= 2)
+                {
+                    sum += positiveList[i] * positiveList[i - 1];
+                }
+                sum += positiveList[0];
+            }
+            return sum;
+        }
+        int MaximalSum(int[] data)
+        {
+            int sum = 0;
+            bool isZero = false;
+            var NegativeList = new List<int>();
+            var PositiveList = new List<int>();
+
+ 
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] < 0)
+                {
+                    NegativeList.Add(data[i]);
+                }
+                else if (data[i] == 0) isZero = true;
+                else if (data[i] == 1) sum++;
+                else
+                {
+                    PositiveList.Add(data[i]);
+                }
+            }
+            if (NegativeList.Count > 0) sum += GetNegativeSum(NegativeList, isZero);
+            if (PositiveList.Count > 0) sum += GetPositiveSum(PositiveList);
+            if (data.Length == 0)
+                Console.WriteLine("Enter More than one Value");
+            return sum;
+        }
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            String input = Console.ReadLine();
+            CreatePairs createPairs = new CreatePairs();
+            do
+            {
+                int[] data = Array.ConvertAll<string, int>(input.Split(','), delegate (string s) { return Int32.Parse(s); });
+                Console.WriteLine(createPairs.MaximalSum(data));
+                input = Console.ReadLine();
+            } while (input != "*");
+        }
+        #endregion
     }
 }
